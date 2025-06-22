6022ea4287d37828a915753447db40d2fac99142
ShreyaKurry13 <ShreyaKurry13@gmail.com>
2025-06-22 16:09:32 +0530

    practice

--- my-practice-app/src/components/AddCars.js
+++ my-practice-app/src/components/AddCars.js
@@ -1,7 +1,39 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useRef } from 'react'
 
 export default function AddCars() {
+
+  const ref1 = useRef();
+  const ref2 = useRef();
+  const ref3 = useRef();
+  const ref4 = useRef();
+  const ref5 = useRef();
+
+  function addRecord(){
+    // alert();
+    console.log(ref1.current.value);
+    var dataStore = {
+      carId:ref1.current.value,
+      make:ref2.current.value,
+      model:ref3.current.value,
+      price:ref4.current.value,
+      year:ref4.current.value,
+    }
+
+    var result = axios.post('http://localhost:8989/cars-api',dataStore);
+    console.log(result);
+    alert("Car Added Successfully");
+  }
+
   return (
-    <div>AddCars</div>
+    <div className="container">
+      <h1>Add Car</h1>
+      <input ref={ref1} type="text" placeholder='Car Id'/><br /><br />
+      <input ref={ref2} type="text" placeholder='Car Name'/><br /><br />
+      <input ref={ref3} type="text" placeholder='Car Model'/><br /><br />
+      <input ref={ref4} type="text" placeholder='Price'/><br /><br />
+      <input ref={ref5} type="text" placeholder='Release Year'/><br /><br />
+      <button className='btn btn-warning' onClick={addRecord}>Add Restaurant</button>
+  </div>
   )
 }
--- my-practice-app/src/components/ShowCars.js
+++ my-practice-app/src/components/ShowCars.js
@@ -1,7 +1,52 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 export default function ShowCars() {
+  var [apidata, setApidata] = useState([]);
+
+  useEffect(() => {
+    console.log("component mounted");
+    console.log("call api");
+    axios
+      .get("http://localhost:8989/cars-api")
+      .then((result) => {
+        console.log(result);
+        console.log(result.data);
+        setApidata(result.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
-    <div>ShowCars</div>
-  )
+    <div className="container">
+      <h1>Data From API</h1>
+
+      <div className="table-responsive">
+        <table className="table table-striped table-bordered table-hover">
+          <thead className="table-dark">
+            <tr>
+              <th scope="col">Id</th>
+              <th scope="col">Make</th>
+              <th scope="col">Model</th>
+              <th scope="col">Price</th>
+              <th scope="col">Release Year</th>
+            </tr>
+          </thead>
+          <tbody>
+            {apidata && apidata.map((value) => (
+                <tr key={value.carId}>
+                  <td>{value.carId}</td>
+                  <td>{value.make}</td>
+                  <td>{value.model}</td>
+                  <td>{value.price}</td>
+                  <td>{value.year}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
