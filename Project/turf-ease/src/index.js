a58ebd225e7909b2948d6e22cd6426302add4e7a
ShreyaKurry13 <shreyakurry13@gmail.com>
2025-07-30 12:03:05 +0530

    updated

--- Project/turf-ease/src/index.js
+++
@@ -1,5 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import projectRoute from './projectRoute';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( projectRoute );
